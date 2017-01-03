4c4b10ae22505417fad5fa58177f65a89fc1aa5c
wuxian.zhang <wuxian.zhang@happygorgi.com>
2017-01-03 15:01:50 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jack482653/pttCrawler/ptt"
+)
+
+func main() {
+	var a ptt.Crawler = &ptt.Article{}
+	err := a.Parse("https://www.ptt.cc/bbs/Gossiping/M.1483256619.A.753.html")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(a)
+}
