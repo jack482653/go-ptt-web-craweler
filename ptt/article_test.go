3a863091cb6042f55c0d2c0064e1dd11f8cbc2d4
wuxian.zhang <wuxian.zhang@happygorgi.com>
2017-02-06 15:37:54 +0800

    add tests of invalid url input

--- ptt/article_test.go
+++ ptt/article_test.go
@@ -53,3 +53,32 @@ func TestNewArticle(t *testing.T) {
 		})
 	}
 }
+
+func TestNewArticleWithWrongUrl(t *testing.T) {
+	type args struct {
+		url string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *Article
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"Wrong Url 1", args{""}, nil, true},
+		{"Wrong Url 2", args{"123456"}, nil, true},
+		{"Wrong Url 3", args{"postgres://user:pass@host.com:5432/path?k=v#f"}, nil, true},
+		{"Wrong Url 4", args{"https://www.google.com"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewArticle(tt.args.url)
+			if tt.wantErr {
+				st.Refute(t, err, nil)
+			} else {
+				st.Assert(t, err, nil)
+			}
+			st.Assert(t, got, tt.want)
+		})
+	}
+}
