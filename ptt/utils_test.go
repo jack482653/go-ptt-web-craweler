03431776fe7afd2b7c27c0a88b9868aac6908bc9
wuxian.zhang <wuxian.zhang@happygorgi.com>
2017-02-08 13:42:42 +0800

    add test of random special chars

--- ptt/utils_test.go
+++ ptt/utils_test.go
@@ -20,8 +20,9 @@ func TestIsUrlValid(t *testing.T) {
 		{"Normal Url 1", args{"https://www.ptt.cc/bbs/Key_Mou_Pad/M.1486458543.A.4A9.html"}, true, false},
 		{"Wrong Url 1", args{""}, false, true},
 		{"Wrong Url 2", args{"123456"}, false, true},
-		{"Wrong Url 3", args{"postgres://user:pass@host.com:5432/path?k=v#f"}, false, true},
-		{"Wrong Url 4", args{"https://www.google.com"}, false, true},
+		{"Wrong Url 3", args{"!$#&^%&^&^%%"}, false, true},
+		{"Wrong Url 4", args{"postgres://user:pass@host.com:5432/path?k=v#f"}, false, true},
+		{"Wrong Url 5", args{"https://www.google.com"}, false, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
